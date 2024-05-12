f9e8e30d5d0a798f1f9ae94b77632735f6a7f37d
Dadi Atar <dadi@montara.io>
2024-05-12 22:59:49 +0300

    json output is working with a web server

--- src/main.py
+++ src/main.py
@@ -1,7 +1,94 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import subprocess
 import webbrowser
 import jsonlines
 import os
+from urllib.parse import urlparse
+import threading
+
+
+html_content = """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Montara report</title>
+            </head>
+            <body>
+                <h1>Montara report</h1>
+                <script>
+                async function fetchJSONL(url) {
+                    const jsonArray = [];
+                    const response = await fetch(url);
+                    const reader = response.body.getReader();
+                    const decoder = new TextDecoder('utf-8');
+                    let chunk = await reader.read();
+                    let result = '';
+                    
+                    while (!chunk.done) {
+                        const text = decoder.decode(chunk.value, { stream: true });
+                        result += text;
+                        const lines = result.split('\\n');
+                        result = lines.pop();  // In case the last chunk ends with an incomplete line
+                        for (const line of lines) {
+                            // Process each line as JSON
+                            try {
+                                const json = JSON.parse(line.trim());
+                                typeof json === 'object' && jsonArray.push(json);
+                            } catch (error) {
+                            }
+                        }
+                        chunk = await reader.read();
+                    }
+                    
+                    // Process the remaining chunk
+                    const text = decoder.decode(chunk.value, { stream: true });
+                    if (text) {
+                        result += text;
+                    }
+                    console.log(jsonArray);
+                }
+                window.onload = () => {
+                    fetchJSONL('/montara_target/output.jsonl');
+                    setInterval(() => {
+                        fetchJSONL('/montara_target/output.jsonl');
+                    }, 2000);
+                    
+                };
+                </script>
+            </body>
+            </html>
+"""
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        print(parsed_path.path)
+        # Open and read the file
+        file_content = html_content.encode()
+        # if parsed file path is not empty
+        if parsed_path.path != "/" and parsed_path.path != "":
+            file_path = parsed_path.path[1:]
+            if os.path.exists(file_path):
+                with open(file_path, "rb") as file:
+                    file_content = file.read()
+            else:
+                self.send_response(404)
+                self.end_headers()
+                return
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(file_content)
+
+
+def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Server running on port {port}")
+    httpd.serve_forever()
 
 
 def main():
@@ -20,69 +107,18 @@ def main():
     if not os.path.exists("montara_target"):
         os.makedirs("montara_target")
 
-    print("Writing output to montara_target directory", flush=True)
-    # Create an HTML file
-    with open("montara_target/output.html", "w") as file:
-        # Write the HTML content with the contents from ../web_application/index.html
-        file.write(
-            """
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Montara report</title>
-            </head>
-            <body>
-                <h1>Montara report</h1>
-                <script>
-                async function fetchJSONL(url) {
-                    const response = await fetch(url);
-                    const reader = response.body.getReader();
-                    const decoder = new TextDecoder('utf-8');
-                    let chunk = await reader.read();
-                    let result = '';
-                    
-                    while (!chunk.done) {
-                        const text = decoder.decode(chunk.value, { stream: true });
-                        result += text;
-                        const lines = result.split('\\n');
-                        result = lines.pop();  // In case the last chunk ends with an incomplete line
-                        for (const line of lines) {
-                            // Process each line as JSON
-                            const json = JSON.parse(line);
-                            console.log(json);
-                        }
-                        chunk = await reader.read();
-                    }
-                    
-                    // Process the remaining chunk
-                    const text = decoder.decode(chunk.value, { stream: true });
-                    result += text;
-                    const json = JSON.parse(result);
-                    console.log(json);
-                }
-                window.onload = () => {
-                    fetchJSONL('./output.jsonl');
-                    setInterval(() => {
-                        fetchJSONL('./output.jsonl');
-                    }, 2000);
-                    
-                };
-                </script>
-            </body>
-            </html>
-"""
-        )
+    webbrowser.open_new_tab("http://localhost:8000")
+    # Open run in a different thread
+    run_thread = threading.Thread(target=run)
+    run_thread.start()
 
-    webbrowser.open_new_tab("file://" + os.path.abspath("montara_target/output.html"))
     print("Writing output to montara_target/output.jsonl", flush=True)
     with jsonlines.open("montara_target/output.jsonl", mode="w") as file:
         while process.poll() is None:
             line = process.stdout.readline()
             if line:
                 # Write each line as a JSON object
-                file.write({"output": line.strip() + "\n"})
+                file.write({"output": line.strip()})
                 file._fp.flush()
 
 

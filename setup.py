2114fc7b8ea4639885deac2c624ff66ec174eb57
Dadi Atar <dadi@montara.io>
2024-05-26 08:51:42 +0300

    pip publish code

--- setup.py
+++ setup.py
@@ -2,7 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbt_run_visualizer",
-    version="0.0.1",
+    version="0.1.0",
+    author="Dadi Atar",
+    author_email="dadi@montara.io",
+    description="Stop drilling through dbt logs and start visualizing them",
+    long_description="Stop drilling through dbt logs and start visualizing them",
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=["jsonlines"],
     entry_points={
@@ -10,6 +15,12 @@ setup(
             "mnt=src.main:main",
         ]
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
     package_data={"src": ["main.py", "index.html"]},
     include_package_data=True,
 )

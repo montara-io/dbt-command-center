ee17fdb231943b59ac9d99487e0aec29c36a8dba
Dadi Atar <dadi@montara.io>
2024-07-02 11:48:10 +0300

    chore: Update npm dependency to latest stable version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbt-command-center",
-    version="0.1.19",
+    version="0.1.20",
     author="Montara team",
     author_email="support@montara.io",
     description="Stop drilling through dbt logs and start visualizing them",
